b9ebfa24780346cc2fbb06765d90de945f3c7098
stian <stianbekkelund@hotmail.com>
2022-01-24 13:10:52 +0100

    Update Pagination.js

--- components/Pagination.js
+++ components/Pagination.js
@@ -23,7 +23,9 @@ const Pagination = ({ page }) => {
   return (
     <PaginationStyles>
       <Head>
-        <title>Sick Fits - Page {page} of ___</title>
+        <title>
+          Sick Fits - Page {page} of {pageCount}
+        </title>
       </Head>
       <Link href={`/products/${page - 1}`}>
         <a aria-disabled={page <= 1}>Prev</a>
