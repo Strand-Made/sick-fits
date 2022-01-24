50ba0ac8e9e5667e88497c9ac1b31a75bd320df5
stian <stianbekkelund@hotmail.com>
2022-01-24 13:10:38 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
 import '../components/styles/nprogress.css';
 import withData from '../lib/withData';
+import { CartStateProvider } from '../lib/CartState';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -11,11 +12,13 @@ Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps, apollo }) {
   return (
-    <ApolloProvider client={apollo}>
-      <Page>
-        <Component {...pageProps} />
-      </Page>
-    </ApolloProvider>
+    <CartStateProvider>
+      <ApolloProvider client={apollo}>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </ApolloProvider>
+    </CartStateProvider>
   );
 }
 
